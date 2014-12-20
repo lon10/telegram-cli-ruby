fe817f036b6c8579afe1a0201ead4d1fd89f41fc
Rustam Manakov <lon10@rambler.ru>
2014-12-20 18:41:18 +0300

    Config added

--- teleram_message_service.rb
+++ teleram_message_service.rb
@@ -1,9 +1,12 @@
 require 'singleton'
+require 'yaml'
 
 class TelegramMessageService
   include Singleton
 
-  def send(message, peer)
-    system("telegram-cli -k /Users/rustam/dist/tg/tg-server.pub -W -e 'msg #{peer} #{message}'")
+  TG_CONFIG = YAML.load_file(File.expand_path('../config/telegram.yml', __FILE__))
+
+  def send(message)
+    system("#{TG_CONFIG['telegram_path']} -k #{TG_CONFIG['key_path']} -W -e 'msg #{TG_CONFIG['peer']} #{message}'")
   end
 end
